927616ec76fc3ecfe5bc367bf968c432f6a11f37
Michal Nielubszyc <m.nielubszyc@gmail.com>
2021-02-12 21:53:55 +0100

    3-primary-survey: improved css, added all components of 1st view

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,14 +1,30 @@
 import { Typography } from "@material-ui/core";
 
 export const GeneralInfo = () => {
-    return (
-      <div className="general-info">
-          <Typography variant="h5" color='primary'>What are you looking for?</Typography>
-          <div className='option-buttons'>
-          <button className='general-info-btn' data='active'><Typography variant="h5" color='secondary'>Project Partner</Typography></button>
-          <button className='general-info-btn'><Typography variant="h5" color='secondary'>Project To Join</Typography></button>
-          <button className='general-info-btn'><Typography variant="h5" color='secondary'>Just looking around</Typography></button>
-          </div> 
+  return (
+    <div className="general-info">
+      <div className="first-question">
+        <Typography variant="h5" color="primary">
+          What are you looking for?
+        </Typography>
+        <div className="option-buttons">
+          <button className="general-info-btn">
+            <Typography variant="h5" color="primary">
+              Project Partner
+            </Typography>
+          </button>
+          <button className="general-info-btn">
+            <Typography variant="h5" color="primary">
+              Project To Join
+            </Typography>
+          </button>
+          <button className="general-info-btn">
+            <Typography variant="h5" color="primary">
+              Just looking around
+            </Typography>
+          </button>
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
--- src/components/PrimarySurvey.js
+++ src/components/PrimarySurvey.js
@@ -4,20 +4,26 @@ import logo from '../logo/FindITRotated.png';
 
 import { SurveySteps } from './SurveySteps';
 import { GeneralInfo } from './GeneralInfo';
+import { NextPrevBtns } from './NextPrevBtns';
 
 export const PrimarySurvey = () => {
   return (
     <div className="primary-survey">
       <div className="logo-column"><img src={logo} alt='FindIT-logo'/></div>
       <div className="survey-column">
+        <div>
         <Typography variant="h4" color='primary'>
           Let's get to know each other better!
         </Typography>
         <Typography variant="h5" color='primary'>
           Answer a few simple questions
         </Typography>
+        </div>
+        <div>
         <SurveySteps />
         <GeneralInfo />
+        </div>
+        <NextPrevBtns />
       </div>
       <div className="right-column"></div>
     </div>
