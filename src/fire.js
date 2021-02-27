d237b3d370034057bf7bf4d507d2f2f677e79a84
Daria <de.zalewska@gmail.com>
2021-02-27 01:01:10 +0100

    adding fire with firebase config in src/fire.js

---
+++ src/fire.js
@@ -0,0 +1,14 @@
+// https://www.youtube.com/watch?v=cFgoSrOui2M
+import firebase from firebase
+
+var firebaseConfig = {
+ apiKey: "AIzaSyA0krGAx1NtVOd1cTap5b-h1FKNCfIq4JA",
+ authDomain: "findit-28a91.firebaseapp.com",
+ projectId: "findit-28a91",
+ storageBucket: "findit-28a91.appspot.com",
+ messagingSenderId: "574792841871",
+ appId: "1:574792841871:web:9037a9e96fea73c0744efa"
+}
+
+consr fire = firebase.initializeApp(firebaseConfig);
+export default fire;
