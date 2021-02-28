242c7b1931c58f324517cee587820f00d9887427
Michal Nielubszyc <m.nielubszyc@gmail.com>
2021-02-28 23:04:19 +0100

    feature/3-primary-survey: simple profile page added, connection with survey to be done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { PrimarySurvey } from './views/PrimarySurvey';
-import WelcomePage from './views/WelcomePage';
+import { PrimarySurvey } from "./views/PrimarySurvey";
+import WelcomePage from "./views/WelcomePage";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import {NavBar} from './components/navBar/NavBar'
-import MainPage from './views/MainPage'
+import { NavBar } from "./components/navBar/NavBar";
+import MainPage from "./views/MainPage";
+import { ProfilePage } from "./views/ProfilePage";
 const theme = createMuiTheme({
   palette: {
-      primary: {
-          main: '#fff',
-      },
-      secondary: {
-          main: '#4853A4',
-      },
+    primary: {
+      main: "#fff",
+    },
+    secondary: {
+      main: "#4853A4",
+    },
   },
 });
 
 function App() {
   return (
- 
-    <MainPage/>
-
-//     <PrimarySurvey />
-//     </ThemeProvider> */}
-    </>
+    <ThemeProvider theme={theme}>
+      {/* <ProfilePage />  */}
+      {/* <MainPage /> */}
+      <PrimarySurvey />
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import App from './App';
-
 ReactDOM.render(
   <React.StrictMode>
     <App/>
