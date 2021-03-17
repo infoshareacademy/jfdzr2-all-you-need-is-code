7d96f5301ede378a45f3861a3f4fb9aacbcd9285
Daria <de.zalewska@gmail.com>
2021-03-17 21:24:10 +0100

    better styling

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -21,31 +21,10 @@ import { useState, useRef } from "react";
 import "../../styles/Chat.css";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  chatSection: {
-    width: "100%",
-    height: "80vh",
-  },
-  headBG: {
-    backgroundColor: "#e0e0e0",
-  },
-  borderRight500: {
-    borderRight: "1px solid #e0e0e0",
-  },
-  messageArea: {
-    height: "70vh",
-    overflowY: "auto",
-  },
-});
-
 const auth = fire.auth();
 
 function Chat() {
-  const classes = useStyles();
-  const scroll = useRef()
+  const scroll = useRef();
   const messagesRef = fire.firestore().collection("Messages");
   const query = messagesRef.orderBy("createdAt").limit(250);
   const [messages] = useCollectionData(query, { idField: "id" });
@@ -62,21 +41,13 @@ function Chat() {
       photoURL,
     });
     setFormValue("");
-    scroll.current.scrollIntoView({ bahavior: 'smooth'})
+    scroll.current.scrollIntoView({ bahavior: "smooth" });
   };
 
   return (
     <>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography variant="h5" className="header-message">
-            Chat
-          </Typography>
-        </Grid>
-      </Grid>
-
-      <Grid container component={Paper} className={classes.chatSection}>
-        <Grid item xs={3} className={classes.borderRight500}>
+      <Grid container component={Paper} className="chat-section">
+        <Grid item xs={3} className="border-right500">
           <List>
             <ListItem button key="Auth">
               <ListItemIcon>
@@ -119,10 +90,11 @@ function Chat() {
             </ListItem>
           </List>
         </Grid>
+
         <Grid item xs={9}>
-          <List className={classes.messageArea}>
+          <List className="message-area">
             <ListItem key="1">
-              {/* <section className="chat-section"> */}
+              <section className="chat-section">
                 <main className="chat-main">
                   {messages &&
                     messages.map((msg) => (
@@ -130,30 +102,32 @@ function Chat() {
                     ))}
                   <span className="chat-span" ref={scroll}></span>
                 </main>
-              {/* </section> */}
+              </section>
             </ListItem>
           </List>
-          <Divider />
+        
+        <Divider />
+      
+      <Grid container style={{ padding: "20px" }}>
+        <Grid item xs={11}>
           <form onSubmit={sendMessage}>
-            <Grid container style={{ padding: "20px" }}>
-              <Grid item xs={11}>
-                <TextField
-                  id="outlined-basic-email"
-                  label="Your message"
-                  fullWidth
-                  value={formValue}
-                  onChange={(e) => setFormValue(e.target.value)}
-                />
-              </Grid>
-
-              <Grid xs={1} align="right">
-                <Fab color="primary" aria-label="add">
-                  <SendIcon />
-                </Fab>
-              </Grid>
-            </Grid>
+            <TextField
+              id="outlined-basic-email"
+              label="Your message"
+              fullWidth
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
+            />
           </form>
         </Grid>
+
+        <Grid xs={1} align="right">
+          <Fab color="primary" aria-label="add">
+            <SendIcon />
+          </Fab>
+        </Grid>
+      </Grid>
+      </Grid>
       </Grid>
     </>
   );
@@ -168,10 +142,10 @@ function Chat() {
             className="chat-img"
             src={
               photoURL ||
-              "https://www.kindpng.com/picc/m/22-223941_transparent-avatar-png-male-avatar-icon-transparent-png.png"
+              "https://material-ui.com/static/images/avatar/2.jpg"
             }
           />
-          <p className="chat-text"> {text}</p>
+          <p className="chat-text">{text}</p>
         </div>
       </>
     );
