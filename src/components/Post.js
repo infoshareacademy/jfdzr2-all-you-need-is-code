e1ca73e8bb6a7cb574cfe8813c5b6c78b89a298a
Kamil-PLewka <kamilplewka12a@wp.pl>
2021-03-20 21:51:53 +0100

    handle on likes, format documents

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,63 +1,58 @@
-import '../styles/Post.css'
-import profilePhoto from '../photos/profilePhotos/profilePhoto.jpeg'
+import "../styles/Post.css";
+import profilePhoto from "../photos/profilePhotos/profilePhoto.jpeg";
 // import Share from '../photos/share.png'
-import Likes from '../photos/likes.png'
-import Share from '../photos/share.png'
-import Coment from '../photos/coment.png'
-import fire from '../fire'
-import React, {useState, useEffect} from 'react';
-export default function Post(props){
+import Likes from "../photos/likes.png";
+import Share from "../photos/share.png";
+import Coment from "../photos/coment.png";
+import fire from "../fire";
+import React, { useState, useEffect } from "react";
+export default function Post(props) {
+  const [like, setLike] = useState(0);
+  function handleLike(e) {
+    console.log(e.target.id)
+    const userUid = fire.auth().currentUser.uid.toString();
+    const docRef = fire
+      .firestore()
+      .collection("Posts")
+      .doc(`Post${e.target.id}`);
+    docRef.collection(`Likes`).doc(userUid).set({
+      like: true,
+    });
+  }
 
-const [like, setLike] = useState(0);
-function handleLike(e){
-  const userUid=fire.auth().currentUser.uid.toString()
-  const docRef = fire.firestore().collection("Posts").doc(`Post${e.target.id}`)
-  docRef.collection(`Likes`).doc(userUid).set({
-    like:true
-  })
-   
- }
-  
-  
+  return (
+    <div className="Modal">
+      <div className="modalOfPost">
+        <div className="informationAboutTheWriter">
+          <img className="profile-Post-Photo" src={profilePhoto}></img>
+          <h3 className="nameOfWriter">Amanda Steal</h3>
+          <p className="positionInProgramming">Senior Front end Developer</p>
+        </div>
+        <h2 className="tittle">{props.title}</h2>
+        <div className="contentOfThePost">
+          <p className="textOfPost">{props.text}</p>
 
-
-    return (
-      <div className="Modal">
-        <div className="modalOfPost">
-          <div className="informationAboutTheWriter">
-              <img className="profile-Post-Photo" src={profilePhoto}></img>
-              <h3 className="nameOfWriter">Amanda Steal</h3>
-              <p className="positionInProgramming">Senior Front end Developer</p>
-          </div>
-          <h2 className="tittle">{props.title}</h2>
-            <div className="contentOfThePost">
-                <p className="textOfPost">{props.text}</p>
-                
-            
-               <div className="postStatus">
-                 <button onClick={handleLike}  className="likesSection">
-                  
-                    <img className="likesPhoto" id={props.index} src={Likes}></img>
-                    <p className="likesCounter" >{props.likes}</p>
-                 </button>
-                  <div className="comentSection">
-                    <img className="comentPhoto" src={Coment}></img>
-                    <p >comment</p>
-                  </div>
-                   
-                    <p className="data">23.01.2021 01:00 pm</p>
-               </div>
-               <div className="coment">
-                  <img className="photoOfComentator"src={profilePhoto}></img>
-                  <div className="comentContent">
-                    <h5 className="comentatorName">Hubert Urbański</h5>
-                    <p className="comentText">Lorem ipsum orem ipsum dolor sit</p>
-                  </div>
-               </div>
-               
+          <div className="postStatus">
+            <button onClick={handleLike} className="likesSection">
+              <img className="likesPhoto" id={props.index} src={Likes}></img>
+              <p className="likesCounter">{props.likes}</p>
+            </button>
+            <div className="comentSection">
+              <img className="comentPhoto" src={Coment}></img>
+              <p>comment</p>
             </div>
+
+            <p className="data">23.01.2021 01:00 pm</p>
+          </div>
+          <div className="coment">
+            <img className="photoOfComentator" src={profilePhoto}></img>
+            <div className="comentContent">
+              <h5 className="comentatorName">Hubert Urbański</h5>
+              <p className="comentText">Lorem ipsum orem ipsum dolor sit</p>
+            </div>
+          </div>
         </div>
-        </div>
-    
+      </div>
+    </div>
   );
 }
--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,72 +1,102 @@
-import  {NavBar} from '../components/navBar/NavBar';
-import "../styles/MainPage.css"
-import Post from '../components/Post'
-import {MainPageWrapper} from '../common/MainPageWrapper'
-import ModalToCreatePost from '../components/ModalToCreatePost'
-import React, {useState, useEffect} from 'react';
-import fire from '../fire';
-import { DeveloperModeSharp, PostAddSharp, Unsubscribe } from '@material-ui/icons';
-import { propTypes } from 'react-bootstrap/esm/Image';
-import { Link } from 'react-router-dom'
+import { NavBar } from "../components/navBar/NavBar";
+import "../styles/MainPage.css";
+import Post from "../components/Post";
+import { MainPageWrapper } from "../common/MainPageWrapper";
+import ModalToCreatePost from "../components/ModalToCreatePost";
+import React, { useState, useEffect } from "react";
+import fire from "../fire";
+import {
+  DeveloperModeSharp,
+  PostAddSharp,
+  Unsubscribe,
+} from "@material-ui/icons";
+import { propTypes } from "react-bootstrap/esm/Image";
+import { Link } from "react-router-dom";
 export default function MainPage() {
-      const [lenght,setLenght]=useState(0)
-    const [isModalOpen, setIsModalOpen]=useState(false);
-    const [posts,setPosts]=useState([])
-    const handleLogout = () => {
-        fire.auth().signOut();
-      }
-    function toggleModal(){
-            setIsModalOpen(current => !current)
-    }
-    useEffect(() => {
-        const unsubscribe = fire.firestore().collection("Posts").onSnapshot((querySnapshot) => {
-            const posts = []
-            querySnapshot.forEach((doc) => { 
-                
-               posts.push({
-                   id: doc.id,
-                   ...doc.data()
-               })
-            });
-            for(let i=1;i<5;i++){
-                fire.firestore().collection('Posts').doc(`Post${i}`).collection('Likes').get().then(snap=>{
-                    var size=snap.size
-                    var array=posts
-                    var object={likesCounter:size}
-                    array[i-1]={...array[i-1], ...object}
-                    setPosts(array)
-
-            }); 
-            
-            }
-            
-        }); 
-
-    }, [])
-   
-    return (
-        <>
-        <MainPageWrapper>
-            <div className="page">
-            <div className="body">
-        <div id="body"className="bodyOfPage">
-            <div className="buttonSection">
-                    <button onClick={toggleModal} className="btn btn-writeMessage">Write a message</button>
-                    <button className="btn-sortBy">Sort by: <b>Popular</b> v</button>
-                    <button className="btn-sortBy" onClick={handleLogout}><Link to='/'><b>Logout</b></Link></button>
-            </div>
-            </div>
-            {
-                console.log(posts)
-            }
-            {
-                posts.map(post=><div>{<Post key={post.id} title={post.title} text={post.text} likes={post.likesCounter} index={post.id} />}</div>)
-            }
-          
-            <NavBar/>
-            </div>
-            </div>
-            </MainPageWrapper>
-            <ModalToCreatePost isModalOpen={isModalOpen} toggleModal={toggleModal}/>
-    </>)
+  const [lenght, setLenght] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+  function toggleModal() {
+    setIsModalOpen((current) => !current);
   }
+  useEffect(() => {
+    const unsubscribe = fire
+      .firestore()
+      .collection("Posts")
+      .onSnapshot((querySnapshot) => {
+        const posts = [];
+        querySnapshot.forEach((doc) => {
+          posts.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        setPosts(posts)
+        for (let i = 1; i < 5; i++) {
+          fire
+            .firestore()
+            .collection("Posts")
+            .doc(`Post${i}`)
+            .collection("Likes")
+            .onSnapshot((snap) => {
+              let likedByYou = false
+              snap.forEach(doc => {
+                if (doc.id === fire.auth().currentUser.uid) {
+                    likedByYou = true
+                }
+              })
+              setPosts(existingPosts => {
+                const array = [...existingPosts]
+                array[i - 1] = { ...array[i - 1], likesCounter: snap.size, likedByYou };
+                return array;
+              })
+            });
+        }
+      });
+  }, []);
+  return (
+    <>
+      <MainPageWrapper>
+        <div className="page">
+          <div className="body">
+            <div id="body" className="bodyOfPage">
+              <div className="buttonSection">
+                <button onClick={toggleModal} className="btn btn-writeMessage">
+                  Write a message
+                </button>
+                <button className="btn-sortBy">
+                  Sort by: <b>Popular</b> v
+                </button>
+                <button className="btn-sortBy" onClick={handleLogout}>
+                  <Link to="/">
+                    <b>Logout</b>
+                  </Link>
+                </button>
+              </div>
+            </div>
+            {console.log(posts)}
+            {posts.map((post) => (
+              <div>
+                {
+                  <Post
+                    key={post.id}
+                    title={post.title}
+                    text={post.text}
+                    likes={post.likesCounter}
+                    index={post.id}
+                  />
+                }
+              </div>
+            ))}
+
+            <NavBar />
+          </div>
+        </div>
+      </MainPageWrapper>
+      <ModalToCreatePost isModalOpen={isModalOpen} toggleModal={toggleModal} />
+    </>
+  );
+}
