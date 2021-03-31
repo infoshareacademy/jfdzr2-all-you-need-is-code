e7b73f64fd3bf477a8638bcb4d62718fd7ef2a81
Michal Nielubszyc <m.nielubszyc@gmail.com>
2021-03-31 23:20:01 +0200

    users page styling done

--- src/components/profile-page/AllUsersInfo.js
+++ src/components/profile-page/AllUsersInfo.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
-import { Paper, Typography, Button, Avatar } from "@material-ui/core";
+import {
+  Paper,
+  Typography,
+  Button,
+  Avatar,
+  TextField,
+  CircularProgress,
+} from "@material-ui/core";
 import firebase from "../../fire";
 import defaultAvatar from "../../photos/profilePhotos/default.jpg";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   large: {
-    width: theme.spacing(16),
-    height: theme.spacing(16),
+    width: theme.spacing(10),
+    height: theme.spacing(10),
   },
 }));
 
@@ -49,174 +55,151 @@ export const AllUsersInfo = () => {
 
   return (
     <>
-      <TextField
-        name="search"
-        type="search"
-        id="search"
-        variant="outlined"
-        label="Search"
-        value={filter}
-        onChange={handleOnChange}
-        fullWidth
-      />
-
       {state === "initial" && (
-        <div className="profile-sections-container">
+        <div className="loading-page">
           <Paper elevation={3} className="profile-section">
-            <p>Loading your friends</p>
+            <Typography variant="h5">Loading your friends</Typography>
+            <CircularProgress color="secondary" />
           </Paper>
         </div>
       )}
 
       {state === "loaded" && (
-        <div className="profile-sections-container">
-          {allUsersInfo
-            ?.filter((user) => {
-              return user.name !== "";
-            })
-            .filter((user) => {
-              return user.id !== currentUser;
-            })
-            .filter(filterByName)
-            .map((user) => {
-              return (
-                <Paper elevation={3} className="profile-section">
-                  <Avatar
-                    className={classes.large}
-                    src={user?.avatarUrl ? user.avatarUrl : defaultAvatar}
-                  />
-                  <Typography
-                    variant="h6"
-                    color="secondary"
-                    style={{ fontWeight: "bold", margin: "6px 0" }}
-                  >
-                    {user?.name}
-                  </Typography>
+        <div className="users-info-container">
+          <TextField
+            name="search"
+            type="search"
+            id="search"
+            variant="outlined"
+            label="Search"
+            value={filter}
+            onChange={handleOnChange}
+            fullWidth
+            style={{ marginBottom: "30px" }}
+          />
+          <div className="users-profiles-container">
+            {allUsersInfo
+              ?.filter((user) => {
+                return user.name !== "";
+              })
+              .filter((user) => {
+                return user.id !== currentUser;
+              })
+              .filter(filterByName)
+              .map((user) => {
+                return (
+                  <Paper elevation={3} className="user-section">
+                    <Avatar
+                      className={classes.large}
+                      src={user?.avatarUrl ? user.avatarUrl : defaultAvatar}
+                    />
+                    <Typography
+                      variant="body1"
+                      color="secondary"
+                      style={{ fontWeight: "bold", margin: "6px 0" }}
+                    >
+                      {user?.name}
+                    </Typography>
 
-                  <div className="profile-content">
-                    <div className="profile-content-line">
-                      <Typography
-                        variant="body1"
-                        color="inherit"
-                        style={{ fontWeight: "400", marginRight: "6px" }}
-                      >
-                        Experience:
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        color="secondary"
-                        style={{
-                          textTransform: "capitalize",
-                          fontWeight: "900",
-                        }}
-                      >
-                        {user?.experience}
-                      </Typography>
+                    <div className="profile-content">
+                      <div className="profile-content-line">
+                        <Typography
+                          variant="body2"
+                          color="inherit"
+                          style={{ fontWeight: "400", marginRight: "6px" }}
+                        >
+                          Experience:
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="secondary"
+                          style={{
+                            textTransform: "capitalize",
+                            fontWeight: "900",
+                          }}
+                        >
+                          {user?.experience}
+                        </Typography>
+                      </div>
+
+                      <div className="profile-content-line">
+                        <Typography
+                          variant="body2"
+                          color="inherit"
+                          style={{ fontWeight: "400", marginRight: "6px" }}
+                        >
+                          Aim:
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="secondary"
+                          style={{
+                            textTransform: "capitalize",
+                            fontWeight: "900",
+                          }}
+                        >
+                          {user?.purpose?.[0] === "projectpartner" &&
+                            ` Project partner`}
+                          {user?.purpose?.[0] === "projecttojoin" &&
+                            ` Project to join`}
+                          {user?.purpose?.[0] === "lookingaround" &&
+                            ` Just looking around`}
+                        </Typography>
+                      </div>
+
+                      <div className="profile-content-line">
+                        <Typography
+                          variant="body2"
+                          color="inherit"
+                          style={{ fontWeight: "400", marginRight: "6px" }}
+                        >
+                          City:
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="secondary"
+                          style={{
+                            textTransform: "capitalize",
+                            fontWeight: "900",
+                          }}
+                        >
+                          {user?.location}
+                        </Typography>
+                      </div>
                     </div>
 
-                    <div className="profile-content-line">
-                      <Typography
-                        variant="body1"
-                        color="inherit"
-                        style={{ fontWeight: "400", marginRight: "6px" }}
-                      >
-                        Looking for:
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        color="secondary"
-                        style={{
-                          textTransform: "capitalize",
-                          fontWeight: "900",
-                        }}
-                      >
-                        {user?.purpose?.[0] === "projectpartner" &&
-                          ` Project partner`}
-                        {user?.purpose?.[0] === "projecttojoin" &&
-                          ` Project to join`}
-                        {user?.purpose?.[0] === "lookingaround" &&
-                          ` Just looking around`}
-                      </Typography>
+                    <div className="user-page-buttons">
+                      <Link>
+                        <Button
+                          color="primary"
+                          style={{
+                            fontSize: "0.8em",
+                            backgroundColor: "#6C7ED6",
+                            padding: "2px",
+                          }}
+                        >
+                          Profile
+                        </Button>
+                      </Link>
+                      <Link>
+                        <Button
+                          color="primary"
+                          style={{
+                            fontSize: "0.8em",
+                            backgroundColor: "#6C7ED6",
+                            padding: "2px",
+                          }}
+                        >
+                          Message
+                        </Button>
+                      </Link>
                     </div>
-
-                    <div className="profile-content-line">
-                      <Typography
-                        variant="body1"
-                        color="inherit"
-                        style={{ fontWeight: "400", marginRight: "6px" }}
-                      >
-                        Location:
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        color="secondary"
-                        style={{
-                          textTransform: "capitalize",
-                          fontWeight: "900",
-                        }}
-                      >
-                        {user?.location}
-                      </Typography>
-                    </div>                
-                  </div>
-                  <Link>
-                    <Button
-                      color="primary"
-                      style={{ backgroundColor: "#6C7ED6", margin: "6px 0 0" }}
-                    >
-                      Profile
-                    </Button>
-                  </Link>
-                  <Link>
-                    <Button
-                      color="primary"
-                      style={{ backgroundColor: "#6C7ED6", margin: "6px 0 0" }}
-                    >
-                      Message
-                    </Button>
-                  </Link>
-                </Paper>
-              );
-            })}
+                  </Paper>
+                );
+              })}
+          </div>
         </div>
       )}
     </>
   );
 };
-
-// <Paper elevation={3} className="profile-section">
-// <Avatar
-//   className={classes.large}
-//   src={user.avatarUrl ? user.avatarUrl : defaultAvatar}
-// />
-// <Typography
-//   variant="h5"
-//   color="inherit"
-//   style={{ fontWeight: "bold" }}
-// >
-//   {user.name}
-// </Typography>
-// <Typography
-//   variant="h5"
-//   color="inherit"
-//   style={{ textTransform: "capitalize" }}
-// >
-//   Experience: {user.experience}
-// </Typography>
-// <Typography
-//   variant="h5"
-//   color="inherit"
-//   style={{ textTransform: "capitalize" }}
-// >
-//   Looking for: {user.purpose}
-// </Typography>
-// <Typography variant="h5" color="inherit">
-//   Location: {user.location}
-// </Typography>
-// <Button
-//   color="primary"
-//   style={{ backgroundColor: "#6C7ED6" }}
-// >
-//   See more
-// </Button>
-// </Paper>
